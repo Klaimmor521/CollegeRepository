825cf4c3f31875e88dddb05cf5ddb02036c4ac03
Amina <arslanova699@gmail.com>
2024-10-21 11:01:46 +0500

    New changes

--- ForRepository/ForRepository.cpp
+++ ForRepository/ForRepository.cpp
@@ -2,9 +2,15 @@
 #include <string>
 using std::cout;
 
+std::string yesOrNo(bool state)
+{
+	return state ? "yes" : "no";
+}
+
 int main()
 {
-	int number = 3;
+	/*int number = 3;
 	std::string text = "Launch Git! Like I have never done that LOL. But I have done this ";
-	cout << text << number;
+	cout << text << number;*/
+	cout << yesOrNo(0);
 }
